833cf3aeaadf82b6460fff2df1ac95372234cfca
ridwanalmahmud <ridwanalmahmud107@gmail.com>
2025-09-14 18:47:59 +0000

    text

--- src/hello.s
+++ src/hello.s
@@ -15,28 +15,5 @@ _start:
 
     ; exit syscall
     mov rax, 60
-    mov rdi, 0
+    xor rdi, rdi
     syscall
-
-; global -> make a symbol visible to the linker
-; section -> organizes code and data into logical segments with specific permissions
-
-; section .text   -> Executable code (instructions)
-; section .data   -> initialized, read-write data
-; section .bss    -> uinitialized, read-write data
-; section .rodata -> initialized read-only data
-
-; rax -> Syscall number
-; rdi -> 1st argument
-; rsi -> 2nd  ""
-; rdi -> 3rd  ""
-; r10 -> 4th  ""
-; r8  -> 5th  ""
-; r9  -> 6th  ""
-
-; db  -> Define byte
-; dw  -> Define word(2 byte)
-; dd  -> Define double word(4 byte)
-; dq  -> Define quad word(8 byte)
-; dt  -> Define ten byte
-; equ -> Define symbolic constants
